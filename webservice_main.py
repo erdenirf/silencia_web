e20920b2f84ab06b474e5b830358e9b4f06c58e4
Erdenirf <erdenirf@gmail.com>
2022-11-29 19:45:30 +0300

    Add files via upload

--- webservice_main.py
+++ webservice_main.py
@@ -32,7 +32,7 @@ model_url = 'http://vectors.nlpl.eu/repository/11/180.zip'
 m = wget.download(model_url)
 model_file = model_url.split('/')[-1]
 with zipfile.ZipFile(model_file, 'r') as archive:
-    stream = archive.open('model.bin')
+    stream = archive.open('model.bin', 'b')
     model = gensim.models.KeyedVectors.load_word2vec_format(stream, binary=True)
 
 
