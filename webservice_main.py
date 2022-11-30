bea561579d899a57c5b39eb45647d311dd5935b5
Erdenirf <erdenirf@gmail.com>
2022-11-30 08:49:30 +0300

    Add files via upload

--- webservice_main.py
+++ webservice_main.py
@@ -110,7 +110,7 @@ def videofiles_to_one(filenames: list, output_filename: str):
     background = cv2.imread( 'background.jpg' , cv2.IMREAD_UNCHANGED)
     cap = cv2_VideoCapture_from_list(filenames)
 
-    writer = cv2.VideoWriter(output_filename, cv2.VideoWriter_fourcc(*'X264'), 30, (320,280))
+    writer = cv2.VideoWriter(output_filename, cv2.VideoWriter_fourcc(*'AVC1'), 30, (320,280))
     #writer = cv2.VideoWriter(output_filename, cv2.VideoWriter_fourcc(*'MP4V'), 30, (320,280))
     #writer = cv2.VideoWriter(output_filename, 0x7634706d, 30, (320,280))
 
